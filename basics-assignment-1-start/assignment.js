6bd07e9d33ebcd7b2ea7f87ab0a921ace2ff8b5e
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-01-13 11:08:00 +0200

    small js task

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,15 @@
+let currentResult = 8;
+
+let Result = 18 + currentResult;
+
+currentResult = currentResult + 10;
+
+currentResult = currentResult - 5;
+
+currentResult = currentResult * 4;
+
+currentResult = currentResult / 3;
+
+alert(currentResult);
+
+alert(Result);
