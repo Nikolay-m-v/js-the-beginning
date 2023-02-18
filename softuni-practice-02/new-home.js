2de5a3f209c595010146185feda9de21108a15fd
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-02-18 11:28:06 +0200

    another task completed

---
+++ softuni-practice-02/new-home.js
@@ -0,0 +1,62 @@
+function newHome(params) {
+  let pricePerRose = 5;
+  let pricePerDahlia = 3.8;
+  let pricePerTulip = 2.8;
+  let pricePerNarcissus = 3;
+  let pricePerGladius = 2.5;
+  let pricePerFlowerYouAreBuying = 0;
+  let typeOfFlowerToBuy = String(params[0]);
+  let amountToBuy = Number(params[1]);
+  let budget = Number(params[2]);
+
+  if (typeOfFlowerToBuy === "Roses") {
+    pricePerFlowerYouAreBuying = 5;
+  } else if (typeOfFlowerToBuy === "Dahlias") {
+    pricePerFlowerYouAreBuying = 3.8;
+  } else if (typeOfFlowerToBuy === "Tulips") {
+    pricePerFlowerYouAreBuying = 2.8;
+  } else if (typeOfFlowerToBuy === "Narcissus") {
+    pricePerFlowerYouAreBuying = 3;
+  } else if (typeOfFlowerToBuy === "Gladius") {
+    pricePerFlowerYouAreBuying = 2.5;
+  }
+
+  let totalSpent = amountToBuy * pricePerFlowerYouAreBuying;
+
+  if (typeOfFlowerToBuy === "Roses" && amountToBuy > 80) {
+    totalSpent -= totalSpent * 0.1;
+  } else if (typeOfFlowerToBuy === "Dahlias" && amountToBuy > 90) {
+    totalSpent -= totalSpent * 0.15;
+  } else if (typeOfFlowerToBuy === "Tulips" && amountToBuy > 80) {
+    totalSpent -= totalSpent * 0.15;
+  } else if (typeOfFlowerToBuy === "Narcissus" && amountToBuy < 120) {
+    totalSpent += totalSpent * 0.15;
+  } else if (typeOfFlowerToBuy === "Gladius" && amountToBuy < 80) {
+    totalSpent += totalSpent * 0.2;
+  }
+
+  let moneyLeftOrNeeded = budget - totalSpent;
+  let moneyLeftOrNeededFormatted = Math.abs(moneyLeftOrNeeded).toFixed(2);
+
+  if (totalSpent > budget) {
+    console.log(
+      "Not enough money, you need " + moneyLeftOrNeededFormatted + " leva more."
+    );
+  } else {
+    console.log(
+      "Hey, you have a great garden with " +
+        amountToBuy +
+        " " +
+        typeOfFlowerToBuy +
+        " and " +
+        moneyLeftOrNeededFormatted +
+        " leva left."
+    );
+  }
+
+  console.log();
+}
+
+newHome(["Roses", "55", "250"]);
+newHome(["Tulips", "88", "260"]);
+newHome(["Narcissus", "119", "360"]);
