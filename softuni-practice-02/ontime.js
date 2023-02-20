111603a31a40976877d362bd8a4d888b86d10cf7
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-02-20 21:09:48 +0200

    added william fix

--- softuni-practice-02/ontime.js
+++ softuni-practice-02/ontime.js
@@ -2,50 +2,66 @@
 
 function onTimeForExam(params) {
   let hourExamStarts = Number(params[0]);
-  let minuteExamStarts = Number(params[1]);
+  let minutesExamStarts = Number(params[1]);
   let hourStudentArrives = Number(params[2]);
-  let minuteStudentArrives = Number(params[3]);
+  let minutesStudentArrives = Number(params[3]);
 
-  let examTime = hourExamStarts * 60 + minuteExamStarts;
-  let comeTime = hourStudentArrives * 60 + minuteStudentArrives;
-  let minutesDifference = comeTime - examTime;
-  let hours = Math.floor(Math.abs(minutesDifference / 60));
-  let minutes = Math.abs(minutesDifference % 60);
+  let examTimeInMinutes = hourExamStarts * 60 + minutesExamStarts;
+  let arrivalTimeInMinutes = hourStudentArrives * 60 + minutesStudentArrives;
 
-  if (comeTime === examTime) {
-    console.log("On time");
-  } else if (minutesDifference < -30) {
+  let minutesDifference = arrivalTimeInMinutes - examTimeInMinutes;
+
+  let cameBeforeExam = minutesDifference < 0;
+  let cameAfterExam = minutesDifference > 0;
+  let onTime = minutesDifference === 0;
+
+  let hoursEarlyOrLate = Math.floor(Math.abs(minutesDifference / 60));
+  let minutesEarlyOrLate = minutesDifference % 60;
+
+  if (
+    cameBeforeExam &&
+    (Math.abs(minutesEarlyOrLate) > 30 || hoursEarlyOrLate >= 1)
+  ) {
     console.log("Early");
-  } else if (minutesDifference < 0) {
-    console.log("On time");
-  } else if (minutesDifference == 0) {
+  } else if (onTime || (cameBeforeExam && Math.abs(minutesEarlyOrLate) <= 30)) {
     console.log("On time");
   } else {
     console.log("Late");
   }
 
-  if (hours > 0) {
-    if (minutes < 10) {
-      console.log(hours + ":0" + minutes + " hours");
-    } else {
-      console.log(hours + ":" + minutes + " hours");
+  if (!onTime) {
+    if (cameBeforeExam) {
+      if (hoursEarlyOrLate >= 1) {
+        console.log(
+          hoursEarlyOrLate +
+            ":" +
+            Math.abs(minutesEarlyOrLate) +
+            " hours before the start"
+        );
+      } else {
+        console.log(Math.abs(minutesEarlyOrLate) + " minutes before the start");
+      }
+    } else if (cameAfterExam) {
+      if (hoursEarlyOrLate >= 1) {
+        console.log(
+          hoursEarlyOrLate +
+            ":" +
+            Math.abs(minutesEarlyOrLate) +
+            " hours after the start"
+        );
+      } else {
+        console.log(Math.abs(minutesEarlyOrLate) + " minutes after the start");
+      }
     }
-  } else {
-    console.log(minutes + " minutes");
-  }
-  if (minutesDifference < 0) {
-    console.log(" before the start");
-  } else {
-    console.log(" after the start");
   }
 }
 
-onTimeForExam(["9", "30", "9", "50"]);
-onTimeForExam(["9", "00", "8", "30"]);
-onTimeForExam(["16", "00", "15", "00"]);
-onTimeForExam(["9", "00", "10", "30"]);
-onTimeForExam(["14", "00", "13", "55"]);
-onTimeForExam(["11", "30", "8", "12"]);
-onTimeForExam(["10", "00", "10", "00"]);
-onTimeForExam(["11", "30", "10", "55"]);
-onTimeForExam(["11", "30", "12", "29"]);
+onTimeForExam(["9", "30", "9", "50"]); // Late
+onTimeForExam(["9", "00", "8", "30"]); // On time
+onTimeForExam(["16", "00", "15", "00"]); // Early
+onTimeForExam(["9", "00", "10", "30"]); // Late
+onTimeForExam(["14", "00", "13", "55"]); // On time
+onTimeForExam(["11", "30", "8", "12"]); // Early
+onTimeForExam(["10", "00", "10", "00"]); // On time
+onTimeForExam(["11", "30", "10", "55"]); // Early
+onTimeForExam(["11", "30", "12", "29"]); // Late
