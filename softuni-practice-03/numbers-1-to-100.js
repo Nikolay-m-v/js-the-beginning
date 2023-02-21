054773986a5b191343706477e590346e87eb3b86
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-02-21 11:49:14 +0200

    added new folder and new task completed

---
+++ softuni-practice-03/numbers-1-to-100.js
@@ -0,0 +1,9 @@
+"use strict";
+Напишете функция, която отпечатва числата от 1 до 100, всяко на нов ред.
+
+function numbers1to100() {
+  for (let i = 1; i <= 100; i++) {
+    console.log(i);
+  }
+}
+numbers1to100();
