3ec751609804b7afe86063d507b58c8f4413c8d6
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-02-22 09:43:26 +0200

    1 more task completed

---
+++ softuni-practice-03/string-to-letters.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Напишете функция, която получава текст (стринг) и печата всеки символ от текста на отделен ред.
+
+function stringToLetters(input) {
+  let word = String(input[0]);
+  for (let i = 0; i < word.length; i++) {
+    let letter = word[i];
+    console.log(letter);
+  }
+}
+
+stringToLetters(["softuni"]);
