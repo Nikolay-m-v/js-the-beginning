5054b64aa969988669408a8a550c7a1ea57336ad
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-02-23 10:28:05 +0200

    second task completed

--- softuni-practice-04/multiply-numbers.js
+++ softuni-practice-04/multiply-numbers.js
@@ -1,1 +1,12 @@
 "use strict";
+
+// Напишете функция, която получава аргумент число от 1 до 10 и принтира таблицата за умножение в конзолата.
+
+function multiplyNumbers(input) {
+  let n = Number(input[0]);
+  for (let i = 1; i <= 10; i++) {
+    console.log(i + " * " + n + " = " + n * i);
+  }
+}
+
+multiplyNumbers(["5"]);
