02e4ce1868f380346f23770a53b37dd5daf24f49
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-02-23 10:23:24 +0200

    added info about first task

--- softuni-practice-04/numbers-to-1000-ending-in-7.js
+++ softuni-practice-04/numbers-to-1000-ending-in-7.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// Напишете функция, която отпечатва числата в диапазона от 1 до 1000, които завършват на 7.
+
 function numbersTo1000By7(input) {
   let n = Number(input[0]);
   for (let i = 7; i < 1000; i++) {
