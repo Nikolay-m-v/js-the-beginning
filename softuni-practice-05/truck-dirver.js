c95ca75d4e541fbe50fa62d4d2bac8ec0f2ce468
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-02-26 10:54:30 +0200

    completed truck driver

--- softuni-practice-05/truck-dirver.js
+++ softuni-practice-05/truck-dirver.js
@@ -20,6 +20,37 @@ function truckDriver(input) {
   let season = String(input[0]);
   let kilometersPerMonth = Number(input[1]);
   let tax = 0.1;
+  let salary = 0;
+  let paidPerKm = 0;
+  let monthsPerSeason = 4;
+
+  if (kilometersPerMonth <= 5000) {
+    if (season === "Spring" || season === "Autumn") {
+      paidPerKm = 0.75;
+    } else if (season === "Summer") {
+      paidPerKm = 0.9;
+    } else {
+      paidPerKm = 1.05;
+    }
+  } else if (kilometersPerMonth > 5000 && kilometersPerMonth <= 10000) {
+    if (season === "Spring" || season === "Autumn") {
+      paidPerKm = 0.95;
+    } else if (season === "Summer") {
+      paidPerKm = 1.1;
+    } else {
+      paidPerKm = 1.25;
+    }
+  } else {
+    paidPerKm = 1.45;
+  }
+
+  salary = kilometersPerMonth * paidPerKm;
+  let salaryForWholeSeason = salary * monthsPerSeason;
+  let totalSalaryAfterTax = salaryForWholeSeason - salaryForWholeSeason * tax;
+
+  console.log(totalSalaryAfterTax.toFixed(2));
 }
 
 truckDriver(["Summer", "3455"]);
+truckDriver(["Winter", "4350"]);
+truckDriver(["Spring", "1600"]);
