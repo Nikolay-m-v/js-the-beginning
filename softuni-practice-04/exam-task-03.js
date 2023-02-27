19db182b7a14ae68d7cc6bc8dfec6fdd6f26229e
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-02-27 11:27:23 +0200

    added 1 more task exam completed

---
+++ softuni-practice-04/exam-task-03.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// Шеф на компания забелязва че все повече служители прекарват  време в сайтове, които ги разсейват.
+// За да предотврати това, той въвежда изненадващи проверки на отворените табове на браузъра на служителите си.
+// Според отворения сайт в таба се налагат следните глоби:
+// "Facebook" -> 150 лв.
+// "Instagram" -> 100 лв.
+// "Reddit" -> 50 лв.
+
+function salary(input) {
+  let amountOfOpenTabs = Number(input[0]);
+  let salary = Number(input[1]);
+  let websiteOpen = String(input[2]);
+  let facebookTax = 150;
+  let instagramTax = 100;
+  let redditTax = 50;
+
+  for (let i = 1; i <= input.length; i++) {
+    let currentWebsiteOpen = input[i];
+    if (currentWebsiteOpen === "Facebook") {
+      salary -= facebookTax;
+    } else if (currentWebsiteOpen === "Instagram") {
+      salary -= instagramTax;
+    } else if (currentWebsiteOpen === "Reddit") {
+      salary -= redditTax;
+    }
+    if (salary <= 0) {
+      console.log("You have lost your salary");
+      break;
+    }
+  }
+  if (salary > 0) {
+    console.log(salary);
+  }
+}
+
+salary([
+  "10",
+  "750",
+  "Facebook",
+  "Dev.bg",
+  "Instagram",
+  "Facebook",
+  "Reddit",
+  "Facebook",
+  "Facebook",
+]);
+salary(["3", "500", "Github.com", "Stackoverflow.com", "softuni.bg"]);
