5a314fc701bc3b535c9379e8c27d93d328262155
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-06 09:52:31 +0200

    redoing tasks

--- udemi-practice-control-structures/practice.js
+++ udemi-practice-control-structures/practice.js
@@ -1,6 +1,20 @@
 function numberZeroToOne(input) {
-  let number = Math.random;
+  let number = Math.random();
   if (number > 0.7) {
     console.log("greater than 0.7");
   }
 }
+
+numberZeroToOne();
+
+function arrayNumbers(input) {
+  for (let i = 0; i < input.length; i++) {
+    console.log(input[i]);
+  }
+
+  for (let i = 1; i >= 0; i--) {
+    console.log(input[i]);
+  }
+}
+
+arrayNumbers([11, 33]);
