94a65e7136e84045dab955a4e349eebbcfa2ca84
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-07 11:03:36 +0200

    started learning nested loops, uploading tasks

---
+++ js-challenges-random/nested-loops/nested-loops-practice.js
@@ -0,0 +1,10 @@
+// Create a new folder named A_nested_loops, this is where you'll save all of your files.
+//Predict what would print when executing each of the snippets below.
+//Test your prediction by typing the snippet into their own files and executing the program. Don't copy and paste.
+// Write the below code into a file named snippet-1.js. Make a prediction for what it will print. Run it.
+
+for (let i = 1; i <= 4; i++) {
+  for (let j = 1; j <= 3; j++) {
+    console.log(i, j);
+  }
+}
