be3d416ce9ab2df407965269b12ad8f60b89e56e
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-08 11:22:21 +0200

    task 3 done

---
+++ js-loops/nested-loops-tasks/nested-loop-task-03.js
@@ -0,0 +1,21 @@
+// Напишете функция, която изчислява колко решения в естествените числа (включително и нулата) има уравнението:
+// x1 + x2 + x3 = n
+// Числото n е цяло число и се въвежда от конзолата.
+
+function combinations(input) {
+  let number = Number(input[0]);
+  let validCombinations = 0;
+
+  for (let i = 0; i <= 99; i++) {
+    for (let j = 0; j <= 99; j++) {
+      for (let k = 0; k <= 99; k++) {
+        if (i + j + k === number) {
+          validCombinations++;
+        }
+      }
+    }
+  }
+  console.log(validCombinations);
+}
+
+combinations([25]);
