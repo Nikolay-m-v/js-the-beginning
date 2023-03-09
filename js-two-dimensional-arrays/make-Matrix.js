0a9e7713be7b2e6a56e1646a8bed350e41bf5e5b
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-09 11:42:51 +0200

    make matrix task completed

--- js-two-dimensional-arrays/make-Matrix.js
+++ js-two-dimensional-arrays/make-Matrix.js
@@ -3,6 +3,39 @@
 // Write a function `makeMatrix(m, n, value)` that accepts three arguments. The function should return
 // a 2-dimensional array of height `m` and width `n` that contains the `value` as every element.
 
-function makeMatrix(input) {}
+function makeMatrix(m, n, value) {
+  let matrix = [];
+
+  for (let i = 0; i < m; i++) {
+    let rows = [];
+    for (let j = 0; j < n; j++) {
+      rows.push(value);
+    }
+    matrix.push(rows);
+  }
+
+  return matrix;
+}
 
 makeMatrix(3, 5, null);
+
+console.log(makeMatrix(3, 5, null));
+// [
+//   [ null, null, null, null, null ],
+//   [ null, null, null, null, null ],
+//   [ null, null, null, null, null ]
+// ]
+
+console.log(makeMatrix(4, 2, "x"));
+// [
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ]
+// ]
+
+console.log(makeMatrix(2, 2, 0));
+// [
+//   [ 0, 0 ],
+//   [ 0, 0 ]
+// ]
