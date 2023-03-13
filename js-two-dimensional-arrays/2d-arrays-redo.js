c0b0655ed2581b0aa43936956ec0c6674674cb0a
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-13 11:58:15 +0200

    task two completed

--- js-two-dimensional-arrays/2d-arrays-redo.js
+++ js-two-dimensional-arrays/2d-arrays-redo.js
@@ -38,3 +38,42 @@ print2d(array2);
 //  11
 //  42
 //  100
+
+// TASK 2
+
+// Write a function `makeMatrix(m, n, value)` that accepts three arguments. The function should return
+// a 2-dimensional array of height `m` and width `n` that contains the `value` as every element.
+
+function makeMatrix(m, n, value) {
+  let array = [];
+
+  for (let i = 0; i < m; i++) {
+    let row = [];
+    array.push(row);
+    for (let j = 0; j < n; j++) {
+      row.push(value);
+    }
+  }
+  console.log(array);
+}
+
+console.log(makeMatrix(3, 5, null));
+// [
+//   [ null, null, null, null, null ],
+//   [ null, null, null, null, null ],
+//   [ null, null, null, null, null ]
+// ]
+
+console.log(makeMatrix(4, 2, "x"));
+// [
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ]
+// ]
+
+console.log(makeMatrix(2, 2, 0));
+// [
+//   [ 0, 0 ],
+//   [ 0, 0 ]
+// ]
