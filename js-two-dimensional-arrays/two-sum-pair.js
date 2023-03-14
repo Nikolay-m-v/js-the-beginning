6527d922d2131612b1240cc12b5adfc31132902b
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-14 19:52:04 +0200

    minor fixes

--- js-two-dimensional-arrays/two-sum-pair.js
+++ js-two-dimensional-arrays/two-sum-pair.js
@@ -24,3 +24,5 @@ console.log(twoSumPairs([2, 3, 4, 6, 5], 8)); // [ [2, 6], [3, 5] ]
 // console.log(twoSumPairs([10, 7, 4, 5, 2], 12)); // [ [10, 2], [7, 5] ]
 // console.log(twoSumPairs([3, 9, 8], 11)); // [ [3, 8] ]
 // console.log(twoSumPairs([3, 9, 8], 10)); // [ ]
+
+// [1, 2, 3, 4]
