d4ca4d653c80fcea86342ad063b190d59f11fb2a
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-14 10:21:07 +0200

    fixing some info on nested loops

--- js-loops/nested-loops/nested-loops-practice-01.js
+++ js-loops/nested-loops/nested-loops-practice-01.js
@@ -1,6 +1,6 @@
 // Create a new folder named A_nested_loops, this is where you'll save all of your files.
-//Predict what would print when executing each of the snippets below.
-//Test your prediction by typing the snippet into their own files and executing the program. Don't copy and paste.
+// Predict what would print when executing each of the snippets below.
+// Test your prediction by typing the snippet into their own files and executing the program. Don't copy and paste.
 // Write the below code into a file named snippet-1.js. Make a prediction for what it will print. Run it.
 
 for (let i = 1; i <= 4; i++) {
