4b49e12b1fe1668351d8db896e1191ba53f35589
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-15 13:28:17 +0200

    task 1 completed

--- js-loops/nested-loops-tasks-2/nested-loops-task-1.js
+++ js-loops/nested-loops-tasks-2/nested-loops-task-1.js
@@ -1,7 +1,46 @@
 // Напишете функция, която получава цяло число n и отпечатва пирамида от числа, като в примерите:
 
 function numberPyramid(n) {
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; n <= i; j++) {}
+  let current = 1;
+  let isBigger = false;
+  let currentLine = "";
+
+  for (let rows = 1; rows <= n; rows++) {
+    for (let cols = 1; cols <= rows; cols++) {
+      if (current > n) {
+        isBigger = true;
+        break;
+      }
+      currentLine += current + " ";
+      current++;
+    }
+    console.log(currentLine);
+    currentLine = "";
+    if (isBigger) {
+      break;
+    }
   }
 }
+
+numberPyramid([7]);
+numberPyramid([15]);
+
+// let current = 1;
+// let isBigger = false;
+// let printCurrentLine = "";
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; n <= i; j++) {
+//     if (current > n) {
+//       isBigger = true;
+//       break;
+//     }
+//     printCurrentLine += current + " ";
+//     current++;
+//   }
+//   console.log(printCurrentLine);
+//   printCurrentLine = "";
+//   if (isBigger) {
+//     break;
+//   }
+// }
