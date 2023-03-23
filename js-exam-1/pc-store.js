de600eecda74b0e0b2e917ee3b21a7913f17434d
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-23 13:37:52 +0200

    task pc store minor fixes additions

--- js-exam-1/pc-store.js
+++ js-exam-1/pc-store.js
@@ -1,12 +1,20 @@
 "use strict";
 
+// Магазин за компютърни игри ви наема за да направите статистика на процента продажби на игрите от
+// последния месец, като изчислите по колко процента от общите продажби са за някоя от игрите.
+// Процентите трябва да бъдат разделени на четири части, три заглавия на игри и всички останали :
+// • Hearthstone
+// • Fornite
+// • Overwatch
+// • Others
+
 function pcStore(input) {
   let index = 0;
   let games = Number(input[index++]);
   let hearthstoneCounter = 0;
   let fortniteCounter = 0;
   let overwatchCounter = 0;
-  let counterstrikeCounter = 0;
+  let othersCounter = 0;
 
   for (let i = 0; i < games; i++) {
     let currentGame = input[index++];
@@ -16,20 +24,20 @@ function pcStore(input) {
       fortniteCounter++;
     } else if (currentGame === "Overwatch") {
       overwatchCounter++;
-    } else if (currentGame === "Counter-strike") {
-      counterstrikeCounter++;
+    } else {
+      othersCounter++;
     }
   }
   let percentageHs = (hearthstoneCounter / games) * 100;
   let percentageFn = (fortniteCounter / games) * 100;
   let percentageOw = (overwatchCounter / games) * 100;
-  let percentageCs = (counterstrikeCounter / games) * 100;
+  let percentageOthers = (othersCounter / games) * 100;
 
   console.log(games);
   console.log("Hearthstone - " + percentageHs.toFixed(2) + "%");
   console.log("Fortnite - " + percentageFn.toFixed(2) + "%");
   console.log("Overwatch - " + percentageOw.toFixed(2) + "%");
-  console.log("Counter-strike - " + percentageCs.toFixed(2) + "%");
+  console.log("Others - " + percentageOthers.toFixed(2) + "%");
 }
 
 pcStore(["4", "Hearthstone", "Fortnite", "Overwatch", "Counter-strike"]);
