41ee999b9b1d65bc731438ea4f8b22747176ec16
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-04-24 12:09:15 +0300

    task done

---
+++ Softuni/01. First Steps In Coding - Lab/concat-text-and-numbers.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function concatTextAndNumbers(input) {
+  console.log(
+    `You are ${input[0]} ${input[1]}, a ${input[2]}-years old person from ${input[3]}`
+  );
+}
+
+concatTextAndNumbers(["Maria", "Ivanova", "20", "Sofia"]);
