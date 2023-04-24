b3baea5790ec8d95e48f417227d79f9e83075b1f
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-04-24 11:48:54 +0300

    massive folder fixes

--- Softuni/softuni-practice/multiplication-table.js
+++
@@ -1,9 +0,0 @@
-// You will receive a number as a parameter. Print the 10 times table for this number.
-
-function multiplicationTable(n) {
-  for (let i = 1; i <= 10; i++) {
-    console.log(n + " * " + i + " = " + n * i);
-  }
-}
-
-multiplicationTable(5);
