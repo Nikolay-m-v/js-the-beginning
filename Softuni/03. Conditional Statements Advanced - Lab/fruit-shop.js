d9f0a3af2cb1cbe27fa39f16fd485ba6f183353f
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-04-26 10:17:34 +0300

    task done

---
+++ Softuni/03. Conditional Statements Advanced - Lab/fruit-shop.js
@@ -0,0 +1,72 @@
+"use strict";
+
+function fruitShop(input) {
+  let fruit = input[0];
+  let day = input[1];
+  let amount = Number(input[2]);
+  let pricePerItem = 0;
+
+  if (
+    day === "Monday" ||
+    day === "Tuesday" ||
+    day === "Wednesday" ||
+    day === "Thursday" ||
+    day === "Friday"
+  ) {
+    switch (fruit) {
+      case "banana":
+        pricePerItem = 2.5;
+        break;
+      case "apple":
+        pricePerItem = 1.2;
+        break;
+      case "orange":
+        pricePerItem = 0.85;
+        break;
+      case "grapefruit":
+        pricePerItem = 1.45;
+        break;
+      case "kiwi":
+        pricePerItem = 2.7;
+        break;
+      case "pineapple":
+        pricePerItem = 5.5;
+        break;
+      case "grapes":
+        pricePerItem = 3.85;
+        break;
+    }
+  }
+
+  if (day === "Saturday" || day === "Sunday") {
+    switch (fruit) {
+      case "banana":
+        pricePerItem = 2.7;
+        break;
+      case "apple":
+        pricePerItem = 1.25;
+        break;
+      case "orange":
+        pricePerItem = 0.9;
+        break;
+      case "grapefruit":
+        pricePerItem = 1.6;
+        break;
+      case "kiwi":
+        pricePerItem = 3;
+        break;
+      case "pineapple":
+        pricePerItem = 5.6;
+        break;
+      case "grapes":
+        pricePerItem = 4.2;
+        break;
+    }
+  }
+
+  let totalCost = amount * pricePerItem;
+  console.log(totalCost);
+}
+
+fruitShop(["apple", "Tuesday", "2"]);
+fruitShop(["apple", "Sunday", "2"]);
